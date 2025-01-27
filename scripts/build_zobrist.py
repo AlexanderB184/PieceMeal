e272a0b18b82c400ee83e6f4d088d8322989b862
AlexanderB184 <a1849988@adelaide.edu.au>
2025-01-27 10:35:43 +1030

    add script to build zobrist piece index lookup table

--- scripts/build_zobrist.py
+++ scripts/build_zobrist.py
@@ -19,9 +19,33 @@ INCLUDES = "#include \"chess.h\"\n"
 ZOBRIST_TABLE_DECL = f"static const zobrist_t zobrist_table[{ROWS}][{COLS}] = {'{'}"
 ZOBRIST_BLACK_TO_MOVE_DECL = "static const zobrist_t zobrist_black_to_move = "
 
+PAWN = 0x1
+KNIGHT = 0x2
+BISHOP = 0x4
+ROOK = 0x8
+QUEEN = BISHOP + ROOK
+KING = 0x10
+
+INDEX_MAP_SIZE = KING + 1
+
+ZOBRIST_PIECE_INDEX_MAP_DECL = f"static const int8_t piece_to_zobrist_index_map[{INDEX_MAP_SIZE}] = {{"
+
+ZOBRIST_PIECE_INDEX_MAP = [-1] * INDEX_MAP_SIZE
+ZOBRIST_PIECE_INDEX_MAP[PAWN] = 0
+ZOBRIST_PIECE_INDEX_MAP[KNIGHT] = 1
+ZOBRIST_PIECE_INDEX_MAP[BISHOP] = 2
+ZOBRIST_PIECE_INDEX_MAP[ROOK] = 3
+ZOBRIST_PIECE_INDEX_MAP[QUEEN] = 4
+ZOBRIST_PIECE_INDEX_MAP[KING] = 5
+
 if __name__ == "__main__":    
     print(HEADER_GUARD_TOP)
     print(INCLUDES)
+    #print(ZOBRIST_PIECE_INDEX_MAP_DECL)
+    #for index in ZOBRIST_PIECE_INDEX_MAP:
+    #    print(f"\t{index},")
+    #print(f"}};\n")
+    
     print(ZOBRIST_BLACK_TO_MOVE_DECL + randomHex64() + ';\n')
     print(ZOBRIST_TABLE_DECL)
     for row in range(ROWS):
